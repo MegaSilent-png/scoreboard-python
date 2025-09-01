4732e9fa742b1e3ee9d59f96e29f5e239b6043d5
Mega <lscomp509@gmail.com>
2025-09-01 18:19:35 -0400

    Added the write function, Initiated switch button.

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from os.path import exists
 import os
 import tkinter as tk
 
+# Score Board Records
+dir_name = "SBR"
+fileNames = ["label", "P1", "scrP1", "P2", "scrP2"]
+PATH = "./" +  dir_name + "/"
+
 def genDir():
-	# Score Board Records
-	dir_name = "SBR"
+
 	try:
 		os.mkdir(dir_name)
 		print(f"{dir_name} directory created.")
@@ -17,10 +21,6 @@ def genDir():
 	except Exception as e:
 		print(f"Error occured: {e}")
 
-	PATH = "./" +  dir_name + "/"
-	
-	fileNames = ["label", "P1", "scrP1", "P2", "scrP2"]
-
 	for file in fileNames:
 		filePath = PATH + file + ".txt"
 		if not exists(filePath):
@@ -30,8 +30,30 @@ def genDir():
 def callback(P):
 	return str.isdigit(P) or P == ""
 
-def writeScore():
+def switchPlayers():
 	pass
+
+def writeScore():
+	global tLabel, p1, p1Scr, p2, p2Scr
+
+	with open(PATH + "label.txt", "w") as f:
+		f.write(str(tLabel.get()))
+	
+	with open(PATH + "P1.txt", "w") as f:
+		f.write(str(p1.get()))
+
+	p1S = p1Scr.get()
+	if callback(p1S):
+		with open(PATH + "scrP1.txt", "w") as f:
+			f.write(str(p1S))
+
+	with open(PATH + "P2.txt", "w") as f:
+		f.write(str(p2.get()))
+
+	p2S = p2Scr.get()
+	if callback(p2S):
+		with open(PATH + "scrP2.txt", "w") as f:
+			f.write(str(p2S))
 
 root = tk.Tk()
 root.title("Score Board Editor")
@@ -39,17 +61,28 @@ height = 400
 width = 600
 root.geometry(str(width) + "x" + str(height))
 
+tLabelL = tk.Label(root, text = "Label")
+tLabelL.pack()
+tLabel = tk.Entry(root, width=20)
+tLabel.pack()
+
 p1L = tk.Label(root, text = "Player 1")
 p1L.pack()
-p1 = tk.Text(root, height=1, width=15)
+p1 = tk.Entry(root, width=15)
 p1.pack()
+p1Scr = tk.Entry(root, width=2)
+p1Scr.pack()
 
 p2L = tk.Label(root, text = "Player 2")
 p2L.pack()
-p2 = tk.Text(root, height=1, width=15)
+p2 = tk.Entry(root, width=15)
 p2.pack()
+p2Scr = tk.Entry(root, width=2)
+p2Scr.pack()
 
-button = tk.Button(root,
+
+
+submitButton = tk.Button(root,
 				   text="Submit",
 				   command=writeScore,
 				   height=1,
@@ -58,7 +91,18 @@ button = tk.Button(root,
 				   width=5,
 				   wraplength=100)
 
-button.pack(side="bottom", padx=20, pady=20)
+submitButton.pack(side="bottom", padx=20, pady=20)
+
+switchButton = tk.Button(root,
+				   text="Switch Players",
+				   command=switchPlayers,
+				   height=1,
+				   padx=10,
+				   pady=5,
+				   width=15,
+				   wraplength=100)
+
+switchButton.pack(side="bottom", padx=20, pady=20)
 
 genDir()
 
